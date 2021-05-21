e29c832baf44f22b5e8a6dd7c4ae2cf606d2a01b
Ana Arine <analucia.arine@gmail.com>
2021-05-21 18:14:33 -0300

    Add constructor and fish file

--- Aquarium/Aquarium.kt
+++ Aquarium/Aquarium.kt
@@ -1,10 +1,7 @@
 package Aquarium
 
-class Aquarium {
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
 
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
 
 //    fun volume(): Int {
 //        return width * height * length / 1000
@@ -13,6 +10,15 @@ class Aquarium {
     var volume : Int
     get() = width * height * length / 1000
     set(value) {height = (value * 1000) / (width * length)}
+
+    var water = volume * 0.9
+
+    constructor(numberOfFish: Int): this() {
+
+        val water: Int = numberOfFish * 2000  //cm3
+        val tank: Double = water + water * 0.1
+        height = (tank / (length * width)). toInt()
+    }
 
 
 
