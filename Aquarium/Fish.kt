b9e9ffa2713d312b05db45c3689fc97fd18a5b3a
Ana Arine <analucia.arine@gmail.com>
2021-05-21 23:10:24 -0300

    Operations with init and constructor

--- Aquarium/Fish.kt
+++ Aquarium/Fish.kt
@@ -6,14 +6,33 @@ class Fish(val friendly: Boolean = true, volumeNeeded: Int) {
     //init works as a constructor and we can put a logic in it to initialize our properties.
 
     init {
-
+        println("First init block")
     }
+
+    constructor() : this(true, 9) {
+        println("Running secondary constructor")
+    }
+
     init {
         if (friendly) {
             size = volumeNeeded
         } else {
-
-        } // if the fish is aggressive  (friendly = false), it will ned twice the minimum volume.
-
+            size = volumeNeeded * 2
+        } // if the fish is aggressive  (friendly = false), it will need twice the minimum volume.
     }
+
+    init {
+        println("constructed fish of size $volumeNeeded final size ${this.size}")
+    }
+
+    init {
+        println("Last init block")
+    }
+}
+
+fun makeDefaultFish() = Fish(true, 50)
+
+fun fishExample() {
+    val fish = Fish(true, 20)
+    println("Is the fish friendly? ${fish.friendly}. It needs volume ${fish.size}")
 }
