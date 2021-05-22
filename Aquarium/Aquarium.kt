2067f50b635653f7c669eada167348c132b7b774
Ana Arine <analucia.arine@gmail.com>
2021-05-22 15:18:56 -0300

    Implement inheritance

--- Aquarium/Aquarium.kt
+++ Aquarium/Aquarium.kt
@@ -1,17 +1,22 @@
 package Aquarium
 
-class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import kotlin.math.PI
+
+//open, to turn the class inheritable
+open class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
 
 
 //    fun volume(): Int {
 //        return width * height * length / 1000
 //    }
 
-    var volume : Int
+
+    //Members are not available for subclassing by default. If needed, let's make the vars "open"
+    open var volume : Int
     get() = width * height * length / 1000
     set(value) {height = (value * 1000) / (width * length)}
 
-    var water = volume * 0.9
+    open var water = volume * 0.9
 
     constructor(numberOfFish: Int): this() {
 
@@ -20,9 +25,14 @@ class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40
         height = (tank / (length * width)). toInt()
     }
 
+}
 
+class TowerTank(): Aquarium() {
 
+    override var water = volume * 0.8
 
-
+    override var volume: Int
+        get() = (width * height * length / 1000 * PI).toInt()
+        set(value) {height = (value * 1000) / (width * length)}
 
 }
